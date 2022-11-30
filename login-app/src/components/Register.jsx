9e43eb32d1cc7fd395865b104a16163b69bcadf9
Noelia <alu0101225555@ull.edu.es>
2022-11-30 16:31:15 +0000

    version 1.2.1 formulario basico

--- login-app/src/components/Register.jsx
+++ login-app/src/components/Register.jsx
@@ -5,6 +5,7 @@ export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,10 +21,16 @@ export const Register = (props) => {
                 <label htmlFor="name">Name</label>
                 <input value={name} type="name" placeholder="Nombre" id="name" name="name"/>
 
+                <label htmlFor="phone">Phone</label>
+                <input value={phone} type="phone" placeholder="645129823" id="phone" name="phone"/>
+
                 <label htmlFor="email">Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
 
                 <label htmlfor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password"/>
+
+                <label htmlfor="password">Retype password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password"/>
 
                 <button type="submit">Sign up</button>
