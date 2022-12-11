e306e77c321d922689a4a942c4413e0cba200e86
Noelia <alu0101225555@ull.edu.es>
2022-12-11 01:52:45 +0000

    test logged añadido

--- frontend/src/test/logged.test.js
+++ frontend/src/test/logged.test.js
@@ -1,12 +1,14 @@
-import { Logeado } from '../components/logeado';
+//import { Logeado } from '../components/logeado';
+import { Logeado } from '/home/usuario/E17/frontend/src/components/Logeado.jsx';
 import { render, screen } from '@testing-library/react';
 
 // hacemos test de Logeado
 // comprobamos que existe la pagina
 describe('Logeado', () => {
+
     test('should render the Name', () => {
-        const { getByLabelText } = render(<Logeado />);
-        const childElement = getByLabelText("Has iniciado sesión correctamente");
-        expect(childElement).toBeTruthy();
+        const view = render(<Logeado />);
+        view.getByText("Has iniciado sesión correctamente");
     });
+
 });
